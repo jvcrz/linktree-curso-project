09cfadc2e6785950501a15911a32a9eb9facd6ad
jvcrz <joaovictormdc99@gmail.com>
2024-03-25 09:59:54 -0300

    Removido header da página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,6 @@ import {
     getDoc
 } from 'firebase/firestore'
 
-import { Header } from '../../components/Header'
 
 interface LinkProps{
     id: string;
@@ -79,7 +78,6 @@ export function Home(){
 
     return(
         <div className="flex flex-col w-full py-4 items-center justify-center">
-            <Header />
             <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Joao</h1>
             <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
